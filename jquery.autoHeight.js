86179ee9b1bae9212c56b7298fd08fa5d6d4ea42
nobleclem <nobleclem@fatalexception.us>
2015-03-16 09:40:06 -0400

    Update jquery.autoHeight.js
    added min/max window width for autoHeight execution.  Updated height check to use outerHeight() instead of just height() to be more accurate.

--- jquery.autoHeight.js
+++ jquery.autoHeight.js
@@ -11,23 +11,44 @@
  * 
  **/
 (function($){
-    $.fn.autoHeight = function(){
+    $.fn.autoHeight = function( options ){
+        defaults = {
+            // only resize if between these heights, set to false to disable (min|max)Height check
+            minWidth: false,
+            maxWidth: false
+        }
+        options = $.extend( defaults, options );
+
         var elements = this;
 
         // re-execute on resize to support responsive patterns
         $(window).resize(function(){
             var maxHeight = 0;
-            
+
             // reset height and recalculate on window resize
             // determine max height of elements,
             // then set all to maxHeight
             elements.each(function(){
                 $(this).height('').each(function(){
-                    if( $(this).height() > maxHeight ) {
-                        maxHeight = $(this).height();
+                    // check for window minWidth
+                    if( options.minWidth && $(window).width() < options.minWidth ) {
+                        return true; // skip
+                    }
+                    // check for window maxWidth
+                    else if( options.maxWidth && $(window).width() > options.maxWidth ) {
+                        return true; // skip
+                    }
+
+                    if( $(this).outerHeight() > maxHeight ) {
+                        maxHeight = $(this).outerHeight();
                     }
                 });
-            }).height( maxHeight );
+            });
+
+            // only reset if we have a maxHeight
+            if( maxHeight ) {
+                elements.height( maxHeight );
+            }                                                                     
         }).trigger('resize');
     };
 }(jQuery));
